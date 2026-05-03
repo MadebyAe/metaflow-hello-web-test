106447ebda803d2504c2161dab709a3c6e88e6af
Ae <hi@angel-estrada.com>
2026-05-02 23:18:02 -0700

    Define types and setup React entry points

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export type Priority = 'none' | 'low' | 'medium' | 'high';
+
+export type Filter = 'all' | 'active' | 'completed';
+
+export interface Task {
+  id: number;
+  text: string;
+  done: boolean;
+  priority: Priority;
+}
